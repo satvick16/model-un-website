499e7538b183db6cd566679e95a4568a2f9ad0d4
Satvick Acharya <satvickacharya16@gmail.com>
2020-11-13 15:29:53 -0500

    db url

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,7 +6,8 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba248'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://jighdpllfrixlw:adf9336c5c2395a561c0b403cf4d7fd95ea8579539c6737f071463e956355c23@ec2-3-218-75-21.compute-1.amazonaws.com:5432/d733jsgo4ibd7u"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
